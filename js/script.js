73c9f1c114e9f1785795f5373d84f281ede65a6c
longlivesquare <stevenduran@gmail.com>
2020-09-22 19:58:49 -0700

    Take-Home

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// This is a single line comment
+/* This comment spans
+several lines
+of code */
+console.log("Steven Duran");
+console.log("I love me some tri tip");
+
+function test() {
+    console.log("I'm in this function");
+}
