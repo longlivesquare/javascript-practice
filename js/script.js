da0bee9ab1ca021609d76f604d7ff78f19eb0a3e
longlivesquare <stevenduran@gmail.com>
2020-09-22 20:06:59 -0700

    Added a button

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 /* This comment spans
 several lines
 of code */
-console.log("Steven Duran");
-console.log("I love me some tri tip");
+
 
 function test() {
-    console.log("I'm in this function");
+    console.log("Steven Duran");
+console.log("I love me some tri tip");
 }
