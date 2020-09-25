53830d4e5d85a0d2a8c242ca9da28883008a3f6e
longlivesquare <stevenduran@gmail.com>
2020-09-24 20:28:48 -0700

    finished with day2 tasks

--- js/script.js
+++ js/script.js
@@ -7,19 +7,24 @@ of code */
 function test() {
     countLetters("supercalifragelousexpialedocious");
     displayAnyNum();
+    displayBigInt(54642165416815361651684231651813216548168816216518168151n);
 }
 
 function countLetters(word) {
     console.log(word.length);
 }
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
 function displayAnyNum(){
-    num = Math.random();
+    num = getRndInteger(1,2**30);
     console.log(num);
-    console.log(typeof(num));
+    console.log(typeof num);
 }
 
 function displayBigInt(myBigInt){
     console.log(myBigInt);
-    console.log(typeof(myBigInt))
+    console.log(typeof myBigInt);
 }
